cd9a63ed4f8d25fbe567b136c66fb17ab4111697
Roman Cardozo <Roman.alejandro.cardozo@gmail.com>
2022-04-29 08:45:57 -0300

    aloritmo

---
+++ seccion-4.1/app.js
@@ -0,0 +1,31 @@
+// Crear un aloritmo, que basado en la nota 
+// imprima si el alumno aprueba la materia 
+// APRUEBA CON 60  
+// Si el alumno tiene nota menor a 50 
+// imprimir un mensaje que diga:
+// Por favor estudie mas
+
+/*let nota = 80; 
+
+if (nota >= 60) {
+    console.log('El alumno aprueba la clase');
+} else {
+    console.log('El alumno NO aprueba la clase');
+}
+
+console.log('Fin del programa');
+*/
+
+let nota = 40; 
+
+if (nota >= 60) {
+    console.log('El alumno aprueba la clase');
+} else if (nota >= 50) {
+    console.log('El alumno NO aprueba la clase');
+} else{
+    console.log('Por favor estudie mas')
+}
+
+console.log('Fin del programa');
+
+
